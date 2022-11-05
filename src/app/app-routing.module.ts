2eef2e482cea4ef3798c087765842a7a069d3477
rai-chatterjee <raimachatterjee.2016@gmail.com>
2022-11-05 23:27:05 +0530

    routing added for terms and condition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { HomeComponent } from './components/home/home.component';
 import { PricingComponent } from './components/pricing/pricing.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ServicesComponent } from './components/services/services.component';
+import { TermsAndConditionComponent } from './components/terms-and-condition/terms-and-condition.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'pricing', component: PricingComponent},
   {path:'about', component: AboutComponent},
   {path:'services', component: ServicesComponent},
-  {path:'products', component: ProductsComponent}
+  {path:'products', component: ProductsComponent},
+  {path: 'terms-and-condition', component: TermsAndConditionComponent}
 ];
 
 @NgModule({
